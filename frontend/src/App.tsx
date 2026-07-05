0a6f72b7a203e633ffa5079fe015ae0db500e812
payback <freedomdaniel989i@gmail.com>
2026-07-05 09:47:08 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import BootScreen from "./components/BootScreen/BootScreen";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <BootScreen />;
+  const [bootComplete, setBootComplete] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setBootComplete(true);
+    }, 5500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return bootComplete ? <Login /> : <BootScreen />;
 }
 
 export default App;
