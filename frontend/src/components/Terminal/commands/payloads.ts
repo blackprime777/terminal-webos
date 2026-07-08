2648bf6bf74172988ef3e3c2d69a66f6b72f3a61
payback <freedomdaniel989i@gmail.com>
2026-07-08 18:29:01 -0400

    Update payloads.ts

--- frontend/src/components/Terminal/commands/payloads.ts
+++ frontend/src/components/Terminal/commands/payloads.ts
@@ -1,1 +1,38 @@
-
+export const payloadStages = [
+  {
+    progress: 0,
+    message: "Initializing zeroday...",
+  },
+  {
+    progress: 10,
+    message: "Loading payload modules...",
+  },
+  {
+    progress: 20,
+    message: "Preparing environment...",
+  },
+  {
+    progress: 35,
+    message: "Synchronizing Zara Core...",
+  },
+  {
+    progress: 50,
+    message: "Running zer0one Phase 1...",
+  },
+  {
+    progress: 65,
+    message: "Running paylaods Phase 2...",
+  },
+  {
+    progress: 80,
+    message: "Verifying zer0one integrity...",
+  },
+  {
+    progress: 90,
+    message: "Finalizing zer0one...",
+  },
+  {
+    progress: 100,
+    message: "zer0one Complete.",
+  },
+];
